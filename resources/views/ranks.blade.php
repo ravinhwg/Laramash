f32171806eb04ee79e69ea257c53e40d0e75568e
ADMIN <ADMIN>
2018-07-02 13:25:56 +0530

    added rankings

--- resources/views/ranks.blade.php
+++ resources/views/ranks.blade.php
@@ -1,17 +1,73 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                    <ol>
-                              @foreach($users as $user)<li>{{$user->name}} </li> @endforeach
+                             
                            
-                          </ol> 
-              
-            </div>
-        </div>
-    </div>
-</div>
+                              @extends('layouts.app')
+
+                              @section('content')
+
+                              <style>body {
+                                  font-family: Tahoma;
+                                  margin: 0;
+                                  padding: 0;
+                                  text-align: center;
+                              }
+                              a {
+                                  text-decoration: none;
+                                  color: darkblue;
+                              }
+                              a:hover {
+                                  text-decoration: underline;
+                              }
+                              img {
+                                  width: 175px;
+                                  height: 250px;
+                                  
+                              }
+                              #header {
+                                  background-color: #8D153A;
+                                  color: #FFBE29;
+                                  padding: 5px;
+                              }
+                              #header a{
+                                  color: #fff;
+                                  text-decoration: none;
+                              }
+                              #main table {
+                                  margin: 0 auto;
+                              }
+                              #footer {
+                                  font: 12px Tahoma;
+                                  margin: 25px 0 50px 0;
+                              }
+                              #footer a {
+                                  margin-right: 10px;
+                              }
+                              
+                              
+                              </style>
+                              
+                              <div id="header">
+                                  <h1><a href="index.html">RANKINGS</a></h1>
+                              </div>
+                              <div id="main">
+                                    @foreach($users as $user)
+                                    <div class="block">
+                                            <img src="https://api.adorable.io/avatars/285/abott@adorable.io.png" alt="">
+                                            <h2>{{$user->name}}</h2>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                          </div>
+                                   
+                                          @endforeach
+                                 
+                              </div>
+                              <div id="footer">
+                                 
+                                 
+                                  
+                                  
+                              </div>
+                              
+                                        
+                             
+                              
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,15 +38,16 @@ img {
 #footer a {
 	margin-right: 10px;
 }</style>
+
 <div id="header">
-	<h1><a href="index.html">🇱🇰</a></h1>
+	<h1><a href="index.html">SRI LANKA POLL</a></h1>
 </div>
 <div id="main">
 	<h3>Were we let in for our looks? No. Will we be judged on them? Yes.</h3>
 	<h2>Who's Hotter? Click to Choose.</h2>
 	<table>
 		<tr>
-                {{$count}}
+               
                 @foreach($pic1 as $pic)
                 @foreach($pic2 as $pics)
        
@@ -63,16 +64,10 @@ img {
 	</table>
 </div>
 <div id="footer">
-	<a href="#">TURKEY</a>
-	<a href="#">GERMANY</a>
-	<a href="#">ITALY</a>
-	<a href="#">RUSSIA</a>
-	<a href="#">RANDOM</a>
-	<br />
-	<a href="index.html">Home</a>
-	<a href="about.html">About</a>
-	<a href="submit.html">Submit</a>
-	<a href="rankings.html">Rankings</a>
+	<h3 color="red"> {{$count}} votes posted </h3>
+	<a href="ranking">View The Rankings</a>
+	
+	
 </div>
 
 @endforeach
